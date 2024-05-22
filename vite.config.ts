be13ae68c0048522dd2a3ae6481b6d63aa0caab1
silverwind <me@silverwind.io>
2024-05-22 22:14:24 +0200

    remove unneccessary opts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,4 @@ import {lib} from "./index.ts";
 
 export default defineConfig(lib({
   url: import.meta.url,
-  build: {
-    rollupOptions: {
-      output: {
-        // for some reason rollup likes to use module name as filename in default config for this module
-        entryFileNames: "[name].js",
-      },
-    }
-  }
 }));
