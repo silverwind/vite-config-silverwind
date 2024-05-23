828f90086b639da2913e5dbb7d56eb5eabfbbbe6
silverwind <me@silverwind.io>
2024-05-23 02:46:15 +0200

    some ts strict fixes

--- index.test.ts
+++ index.test.ts
@@ -3,17 +3,17 @@ import type {LibraryOptions} from "vite";
 import type {OutputOptions} from "rollup";
 
 test("lib", () => {
-  expect(lib().build.target).toEqual("esnext");
-  expect(lib().build.lib).toBeTruthy();
-  expect((lib({url: import.meta.url}).build.lib as LibraryOptions).entry[0]).toBeTruthy();
-  expect(lib().build.rollupOptions.external).toBeArray();
-  expect(lib().build.rollupOptions.external).toBeArray();
-  expect((lib().build.rollupOptions.output as OutputOptions).entryFileNames).toEqual("[name].js");
-  expect((lib({build: {rollupOptions: {output: {entryFileNames: "foo.js"}}}}).build.rollupOptions.output as OutputOptions).entryFileNames).toEqual("foo.js");
-  expect(lib().build.emptyOutDir).toBeTrue();
+  expect(lib()?.build?.target).toEqual("esnext");
+  expect(lib()?.build?.lib).toBeTruthy();
+  expect((lib({url: import.meta.url})?.build?.lib as LibraryOptions)?.entry)?.toBeTruthy();
+  expect(lib()?.build?.rollupOptions?.external).toBeArray();
+  expect(lib()?.build?.rollupOptions?.external).toBeArray();
+  expect((lib()?.build?.rollupOptions?.output as OutputOptions).entryFileNames).toEqual("[name].js");
+  expect((lib({build: {rollupOptions: {output: {entryFileNames: "foo.js"}}}})?.build?.rollupOptions?.output as OutputOptions).entryFileNames).toEqual("foo.js");
+  expect(lib()?.build?.emptyOutDir).toBeTrue();
 });
 
 test("app", () => {
-  expect(app().build.target).toEqual("modules");
-  expect(app().build.emptyOutDir).toBeTrue();
+  expect(app()?.build?.target).toEqual("modules");
+  expect(app()?.build?.emptyOutDir).toBeTrue();
 });
