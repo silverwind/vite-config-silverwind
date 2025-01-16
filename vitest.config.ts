b3d3c5a2fb4a98be67887d515444f3b07f1ed51f
silverwind <me@silverwind.io>
2025-01-16 14:57:05 +0100

    update deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
 import {defineConfig} from "vitest/dist/config.js";
 import {backend} from "vitest-config-silverwind";
 
+// @ts-expect-error type issue in vitest related to "stringify" option
 export default defineConfig(backend({url: import.meta.url}));
